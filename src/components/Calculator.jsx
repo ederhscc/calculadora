d16e193aa987fe68bb0cdd87bf07d8fb9f78c018
Eder Henrique <ederhscc@gmail.com>
2023-12-29 10:52:38 -0300

    structuring the keyboard

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./Calculator.css";
 
 const Calculator = () => {
+  const keypadNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  const operations = ["+", "-", "*", "/"];
+
   return (
     <div className="calculator">
-        <div className="complete-operation">3 + 3 = 6</div>
-        <div className="display">6</div>
-        <div className="buttons"></div>
+      <div className="complete-operation">3 + 3 = 6</div>
+      <div className="display">6</div>
+      <div className="buttons">
+        <button>AC</button>
+        {keypadNumbers.map((num) => (
+            <button key={num}>{num}</button>
+        ))}
+        {operations.map((operation) => (
+            <button key={operation}>{operation}</button>
+        ))}
         <button>=</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Calculator
+export default Calculator;
